ddd0a2b67e89dc5187abd3bd82bbb3ca4d7838d3
Leandro Bento <55304620+leandrobento84@users.noreply.github.com>
2020-06-23 18:05:00 -0300

    Update hello_spec.rb
    Ajustando formato do código.

--- hello_spec.rb
+++ hello_spec.rb
@@ -1,38 +1,25 @@
-#Suite de Teste
-#Cenario: Acessar pagina principal L!
-
 describe 'Meu Primeiro Script', :aaa, :smoke  do
 
     it 'Item Específico Simples', :SP do
         visit'https://www.coscokids.com.br/'# Acessa url / rota princial L!
         sleep 1
-        
+
+        #expect(page.title).to eql 'Training Wheels Protocol' L!
+                        
         visit 'https://www.coscokids.com.br/carrinhos' # Acessa url / rota alternativa L!
         sleep 1
         
         visit 'https://www.coscokids.com.br/atendimento/lojas?totvs=IMP91257' # Acessa url / rota alternativa L!
         select('SP', from: 'state') # Seleciono o UF "SP" do id_state L!
-        sleep 2 # Timer temporário L!
+        sleep 1 #temporario
         
         select('Itapevi', from: 'city') # Seleciono o Cidade "xxx" do id_city L!
-        sleep 2 #temporario
+        sleep 1 #temporario
 
-        fill_in 'endereco', with: 'São Paulo' #Informo a cidade no id_endereco L!
-        sleep 3
+        fill_in 'endereco', with: 'São Paulo' # Informo a cidade no id_endereco 
+        sleep 1
         
         click_button 'Buscar'
         sleep 5
     end
-end
-    
-
-#Verificando nome exibido na pagina acessada "Q.A Ninja"
-        #expect(page.title).to eql 'Training Wheels Protocol'
-        #lobby web
-        
-#describe 'TesteL', :home2 do
-#        it 'xpto' do
-#        visit 'https://www.netshoes.com.br/'
-#        sleep 3
-#        end
-#    end 
+end 
